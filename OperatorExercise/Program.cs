923f8a6bbbfb430a88445ea98a27bfdee4797da1
Doug McPeek <dmcpeek@outlook.com>
2023-04-11 13:19:35 -0600

    Updated operands from * to *=, etc.)

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -6,24 +6,24 @@
         {
             // Exercise 1
             int x = 3, y = 6;
-            Console.WriteLine($"{x} * {y} equals {x * y}");
+            Console.WriteLine($"{x} * {y} equals {x *= y}");
             Console.WriteLine("");
 
             x = 3;
             y = 6;
-            Console.WriteLine($"{x} - {y} equals {x - y}");
+            Console.WriteLine($"{x} - {y} equals {x -= y}");
             Console.WriteLine("");
 
             x = 23581331;
             y = 31415926;
-            Console.WriteLine($"{x} + {y} equals {x + y}");
+            Console.WriteLine($"{x} + {y} equals {x += y}");
 
             Console.WriteLine("----------------------------");
             Console.WriteLine("");
 
             int a= 17;
             int b = 4;
-            int quotient = a / b;
+            int quotient = a /= b;
             int remainder = a % b;
             if (a == 17 && b == 4)
                 Console.WriteLine($"{a} % {b} has a remainder of {remainder}");
